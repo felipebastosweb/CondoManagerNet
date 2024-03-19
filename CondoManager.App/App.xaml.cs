e49a92e6cac9e94cc11835b5b206ee5cb3487c09
Felipe Bastos <felipebastosweb@gmail.com>
2024-03-19 15:13:17 -0300

    Adicionar arquivos de projeto.

---
+++ CondoManager.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CondoManager.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
