d713da38c488e1d4697fc6275928502312a7d261
felipe bastos <felipebastosweb@gmail.com>
2024-03-19 20:28:06 -0300

    definição inicial do Context

--- CondoManager.Api/ApiContext.cs
+++ CondoManager.Api/ApiContext.cs
@@ -2,5 +2,17 @@ using Microsoft.EntityFrameworkCore.SQLServer;
 
 
 namespace CondoManager.Api {
-    
+    public class ApiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Condominium> Condominiums { get; set; }
+        public DbSet<Condominium> CondominiumSupervisors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder
+optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=CondoManagerDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
 }
